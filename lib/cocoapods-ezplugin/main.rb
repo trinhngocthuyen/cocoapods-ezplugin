b6b14a82cb03993901891c3456222e33d5bba30b
Thuyen Trinh <trinhngocthuyen@gmail.com>
2023-01-13 08:38:38 +0700

    Fix typo

--- lib/cocoapods-ezplugin/main.rb
+++ lib/cocoapods-ezplugin/main.rb
@@ -1,6 +1,6 @@
 fname = [
   "ezplugin.rb",
   ".ezplugin.rb",
-].find { |fname| File.exis?(fname) }
+].find { |fname| File.exist?(fname) }
 
 require File.join(".", File.basename(fname, ".*")) unless fname.nil?
